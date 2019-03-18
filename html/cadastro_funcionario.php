38f11589cce9e224fb1053ed46c7de63b3c709ed
Guiazis <guilherme_azis@hotmail.com>
2019-03-18 16:15:06 -0300

    Update cadastro_funcionario.php

--- html/cadastro_funcionario.php
+++ html/cadastro_funcionario.php
@@ -5,7 +5,7 @@
    }
    
       $usuario = "root";
-    	$senha =  "teste";
+    	$senha =  "root";
     	$servidor = "localhost";
     	$bddnome = "wegia";
     	$mysqli = new mysqli($servidor,$usuario,$senha,$bddnome);
