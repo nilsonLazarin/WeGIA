e32960c8aa865e00e7adfa5ca871c9e11bacc75d
root <nilsonmori@yahoo.com>
2020-05-28 20:37:54 +0000

    ajustes MariaDB

---
+++ BD/WeGIA-dadosiniciais.sql
@@ -0,0 +1,5 @@
+use wegia;
+INSERT INTO `pessoa` (`id_pessoa`, `cpf`, `senha`, `nome`) VALUES (NULL, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin');
+INSERT INTO `situacao` (`id_situacao`, `situacoes`) VALUES (NULL, 'Ativo');
+INSERT INTO `situacao` (`id_situacao`, `situacoes`) VALUES (NULL, 'Inativo');
+
