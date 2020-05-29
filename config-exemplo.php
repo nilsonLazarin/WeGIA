967f0f10a2b8720c5cd2863511b62576f2e575a7
Marcio Marqui <matheus.cefetnf@gmail.com>
2020-05-29 17:57:10 -0300

    arquivo de configuração exemplo

---
+++ config-exemplo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *Configuração do WEGIA
+ */
+define( 'DB_NAME', 'wegia' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root123' );
+define( 'DB_HOST', 'localhost');
+define( 'DB_CHARSET', 'utf8');
+
