07d8df2b502d1bc9a688cd33dbe718202f343532
Prof Nilson <nilsonmori@yahoo.com>
2020-05-29 11:34:51 -0300

    arquivo exemplo

---
+++ config-exemplo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *Arquivo Exemplo
+ *Configuração do WEGIA
+ */
+
+define( 'DB_NAME', 'nome_da_base_de_dados' );
+define( 'DB_USER', 'nome_do_usuario_do_mariadb' );
+define( 'DB_PASSWORD', 'senha_do_usuario_banco' );
+define( 'DB_HOST', 'IP_ou_nome_do_servidor_banco_de_dados' );
+
