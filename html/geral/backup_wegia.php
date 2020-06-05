f53ca9fbc27904c618901bce4c05a036a528f52d
Nilson Mori <32855001+nilsonmori@users.noreply.github.com>
2020-06-05 20:33:36 -0300

    Delete backup_wegia.php

--- html/geral/backup_wegia.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if (!isset($_SESSION["usuario"])) {
-        header("Locatiion ../../index.php");
-    }
-    require_once("../../config.php");
-    exec("tar -cvzf ".BKP_DIR.date("Ymd").".site.tar.gz ".caminho);
-    echo("Backup Concluido");
-?>
