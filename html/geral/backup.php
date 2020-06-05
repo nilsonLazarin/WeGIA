fa3cbf868220aaebbd3d47244de0bcf91a86e0a7
Nilson Mori <32855001+nilsonmori@users.noreply.github.com>
2020-06-05 20:22:49 -0300

    Update backup.php

--- html/geral/backup.php
+++ html/geral/backup.php
@@ -5,12 +5,16 @@
     }
     require_once("../../config.php");
     
+    /*Executando Backup do Banco de Dados*/
     exec("mysqldump -u ".DB_USER."  ".DB_NAME." -p".DB_PASSWORD." > ".BKP_DIR.date("Ymd").".bd.sql", $output[0]);
-    
+    /*Executando Backup do Diretório do site*/
     exec("tar -cvzf ".BKP_DIR.date("Ymd").".site.tar.gz ".caminho);
     
+    /*
     echo("<pre>");
     var_dump($output);
     echo("Iniciando Backup");
     header("Location: backup_wegia.php");
+    */
+    echo "Backup realizado com sucesso";
 ?>
