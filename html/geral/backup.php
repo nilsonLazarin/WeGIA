fa4c4c9a7e504b1e33b4e93c1ffdbbe0888e8b01
AntonioHenriqueGF <hgfantonio@gmail.com>
2020-06-09 18:23:06 -0300

    backup.php usando ROOT

--- html/geral/backup.php
+++ html/geral/backup.php
@@ -24,7 +24,7 @@
     exec("mysqldump -u ".DB_USER."  ".DB_NAME." -p".DB_PASSWORD." > ".BKP_DIR.date("YmdHi").".bd.sql", $output[0]);
 
     /*Executando Backup do Diretório do site*/
-    exec("tar -cvzf ".BKP_DIR.date("YmdHi").".site.tar.gz ".caminho);
+    exec("tar -cvzf ".BKP_DIR.date("YmdHi").".site.tar.gz ".ROOT);
     
     /*
     echo("<pre>");
@@ -32,6 +32,5 @@
     echo("Iniciando Backup");
     header("Location: backup_wegia.php");
     */
-    echo "Backup realizado com sucesso";
     header("Location: ../atualizacao_sistema.php?msg=success");
 ?>
