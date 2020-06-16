feef58d7c6bba883cd59b4e45c3b4ae3ad88f8e0
Marcio Marqui <matheus.cefetnf@gmail.com>
2020-06-16 17:50:09 -0300

    Cadastro de sáida agora permite o caractere '-' nos produtos, resolução do bug ao deletar destinos em servidores linux, agora o header mostra o nome do usuário logado

--- html/header.php
+++ html/header.php
@@ -9,7 +9,7 @@
         if(file_exists($config_path)) break;
     }
     require_once($config_path);
-    
+    session_start();
 	require_once "../dao/Conexao.php";
 
 	// Adiciona a Função display_campo($nome_campo, $tipo_campo)
@@ -37,7 +37,7 @@
 					<img src="../img/semfoto.jpg" alt="Joseph Doe" class="img-circle" data-lock-picture="../assets/images/!logged-user.jpg" />
 				</figure>
 				<div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
-					<span class="name">Usuário</span>
+					<span class="name"><?php echo($_SESSION['usuario']); ?></span>
 					<span class="role">Funcionário</span>
 				</div>
 				<i class="fa custom-caret"></i>
