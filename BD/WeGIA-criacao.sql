93f8f2a4c02cc983c4976a3d57a6e686cb2f0f23
Ferdinando-GitHub <57322264+Ferdinando-GitHub@users.noreply.github.com>
2020-06-18 15:21:09 -0300

    id_pessoa unique e limit 1

--- BD/WeGIA-criacao.sql
+++ BD/WeGIA-criacao.sql
@@ -859,7 +859,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `wegia`.`socio` (
   `id_socio` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_pessoa` INT(11) NOT NULL,
+  `id_pessoa` INT(11) NOT NULL UNIQUE,
   `id_sociostatus` INT NOT NULL,
   `id_sociotipo` INT NOT NULL,
   `email` VARCHAR(256) NULL,
@@ -1266,10 +1266,10 @@ complemento)
 values(nome, sobrenome, cpf, telefone,data_nascimento,cep,estado,cidade,bairro,logradouro,numero_endereco,complemento);
 
 insert ignore into socio(id_pessoa, id_sociostatus, id_sociotipo, email)
-values ((SELECT id_pessoa FROM pessoa WHERE pessoa.cpf=cpf), id_sociostatus, id_sociotipo, email);
+values ((SELECT id_pessoa FROM pessoa WHERE pessoa.cpf=cpf limit 1), id_sociostatus, id_sociotipo, email);
 
 insert into log_contribuicao(id_socio, ip, data, hora, id_sistema)
-values((SELECT id_socio FROM socio, pessoa WHERE pessoa.id_pessoa=socio.id_pessoa AND pessoa.cpf=cpf), ip, data, hora, id_sistema);
+values((SELECT id_socio FROM socio, pessoa WHERE pessoa.id_pessoa=socio.id_pessoa AND pessoa.cpf=cpf limit 1), ip, data, hora, id_sistema);
 
 END$$
 
