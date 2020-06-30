c451149253712830132bd2630a61127fd98ed2de
AntonioHenriqueGF <hgfantonio@gmail.com>
2020-06-30 00:55:24 -0300

    Conexao.php

--- html/atualizacao_sistema.php
+++ html/atualizacao_sistema.php
@@ -116,8 +116,6 @@
                 <!-- Caso as alterações feitas sejam feitas com sucesso -->
 				<?php if (isset($_GET['msg'])){ if ($_GET['msg'] == 'success'){ echo('<div class="alert alert-success"><i class="fas fa-check mr-md"></i><a href="#" class="close" onclick="closeMsg()" data-dismiss="alert" aria-label="close">&times;</a>'. ($_GET["sccs"] ? $_GET["sccs"] : "Operação concluida com sucesso!") . '<pre>'.base64_decode($_GET['log']).'</pre></div>');}}?>
 
-
-
 				<!-- Caso haja um erro fatal na alteração dos dados -->
 				<?php if (isset($_GET['msg'])){ if ($_GET['msg'] == 'error'){ echo('<div class="alert alert-danger"><i class="fas fa-exclamation-triangle mr-md"></i><a href="#" class="close" onclick="closeMsg()" data-dismiss="alert" aria-label="close">&times;</a>'. ($_GET["err"] ? $_GET["err"] : "Houve um erro ao atualizar o sistema") .'</div>');}}?>
 				
