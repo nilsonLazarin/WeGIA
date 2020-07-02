ea9f831e09a7eae673ebf2303fa903adaff43786
cynthiaAJardim <59966957+cynthiaAJardim@users.noreply.github.com>
2020-07-02 13:08:13 -0300

    Update logo_titulo.php

--- html/contribuicao/php/logo_titulo.php
+++ html/contribuicao/php/logo_titulo.php
@@ -7,17 +7,18 @@ include("conexao.php");
     $selectImg = "SELECT imagem.imagem, imagem.tipo FROM imagem, campo_imagem, tabela_imagem_campo WHERE campo_imagem.id_campo=tabela_imagem_campo.id_campo AND imagem.id_imagem=tabela_imagem_campo.id_imagem AND campo_imagem.nome_campo='Logo'";
     $query = mysqli_query($conexao, $selectImg);
     $arquivo = mysqli_fetch_row($query);
+    
+    $tipo =$arquivo[1];
 
     $imagem = $arquivo[0];
-    $tipo =".".$arquivo[1];
-   
+    
     $selectPa = "SELECT paragrafo FROM selecao_paragrafo WHERE nome_campo = 'ContribuiçãoMSG'";
     $queryP = mysqli_query($conexao, $selectPa);
     $paragrafo = mysqli_fetch_row($queryP);
 
     $texto = $paragrafo[0];
 
-   echo $logo= '<img src=data:image/png;base64,'.base64_encode($imagem.$tipo).'">'."paragrafo:";
+    echo $logo= '<img src=data:image/'.$tipo.';base64,'.$imagem.'>'."paragrafo:";
     echo $texto;
     
 ?>
