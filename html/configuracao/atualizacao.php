85e4650d6cc1bc430715a252289938a7bd52e764
AntonioHenriqueGF <hgfantonio@gmail.com>
2020-09-16 06:33:59 -0300

    Atualização Debug

--- html/configuracao/atualizacao.php
+++ html/configuracao/atualizacao.php
@@ -21,6 +21,8 @@
     require_once($config_path);
     }
 
+    define("DEBUG_MODE", true);
+
     define("DAY_TIME", date("YmdHi")); // Data e hora em formato AAAAMMDDHHII (I = minuto)
     define("BD_BKP", BKP_DIR.date("YmdHi").".bd.sql"); //Caminho para arquivo de backup temporário do Banco de dados
     define("PAGE_BKP", BKP_DIR.date("YmdHi").".site.tar.gz"); //Caminho para arquivo de backup temporário da página
@@ -44,24 +46,37 @@
 
     function gitPull() {
         //$x = exec("cd ".ROOT." && git fetch --all && git reset --hard origin/master && git pull https://github.com/nilsonmori/WeGIA.git master", $y);
-        exec("git -C ".ROOT." pull", $output);
+        $output = array();
+        exec("git -C ".ROOT." pull 2>&1", $output);
         return $output;
     }
 
     
     $output = gitPull();
+    if (DEBUG_MODE) {
+        var_dump("git -C ".ROOT." pull 2>&1", $output);
+        die();
+    }
     if ($output) {
-        $log = "Status da atualização: \n";
-        foreach ($output as $value){
-            $log = $log . $value . "\n";
-        }
-        $_SESSION['log'] = $log;
-        if (tempBackup()){
-            // header("Location: ./configuracao_geral.php?msg=success&sccs=Backup realizado e Atualização concluída!&log=".base64_encode($log));
-            header("Location: ./configuracao_geral.php?tipo=success&mensagem=Backup realizado e Atualização concluída!");
-        }else{
-            // header("Location: ./configuracao_geral.php?msg=warning&warn=Atualização concluída, mas houve um erro ao realizar o backup (Sistema compatível: Linux, Seu Sistema: ".PHP_OS.")!&log=".base64_encode($log));
-            header("Location: ./configuracao_geral.php?tipo=warning&mensagem=Atualização concluída, mas houve um erro ao realizar o backup (Sistema compatível: Linux, Seu Sistema: ".PHP_OS.")!");
+        if (sizeof($output) == 1){
+            if ($output[0] == 'Already up to date.'){
+                header("Location: ./configuracao_geral.php?msg=success&sccs=O sistema já está atualizado!");
+            }else{
+                header("Location: ./configuracao_geral.php?msg=error&err=Houve um erro ao executar o comando git -C ".ROOT." pull");
+            }
+        }elseif (sizeof($output) != 0) {
+            $log = "Status da atualização: \n";
+            foreach ($output as $value){
+                $log = $log . $value . "\n";
+            }
+            $_SESSION['log'] = $log;
+            if (tempBackup()){
+                // header("Location: ./configuracao_geral.php?msg=success&sccs=Backup realizado e Atualização concluída!&log=".base64_encode($log));
+                header("Location: ./configuracao_geral.php?tipo=success&mensagem=Backup realizado e Atualização concluída!");
+            }else{
+                // header("Location: ./configuracao_geral.php?msg=warning&warn=Atualização concluída, mas houve um erro ao realizar o backup (Sistema compatível: Linux, Seu Sistema: ".PHP_OS.")!&log=".base64_encode($log));
+                header("Location: ./configuracao_geral.php?tipo=warning&mensagem=Atualização concluída, mas houve um erro ao realizar o backup (Sistema compatível: Linux, Seu Sistema: ".PHP_OS.")!");
+            }
         }
     } else {
         // header("Location: ./configuracao_geral.php?msg=error&err=Houve um erro ao executar o comando git -C ".ROOT." pull");
