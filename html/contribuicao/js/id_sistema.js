b3ffe54bd10fa989142e087764e227c7be483165
cynthiaAJardim <cynthiastudytime@gmail.com>
2020-09-17 17:17:19 -0300

    inserção de data de vencimento e valor no banco, correção do ultimo form de doação, correção de cadastro de sócios no BD

--- html/contribuicao/js/id_sistema.js
+++ html/contribuicao/js/id_sistema.js
@@ -1,7 +1,7 @@
 function retorna_id(nome_sistema)
 {
     var nome_sistema = nome_sistema.toUpperCase();
-    $.post("id_sistema.php",{'nome_sistema':nome_sistema}).done
+    $.post("./php/id_sistema.php",{'nome_sistema':nome_sistema}).done
     (function(data)
     {   
         var id = data; 
@@ -11,6 +11,8 @@ function retorna_id(nome_sistema)
         preencher(id);
         preenche_dados_cartao(id);
         //inserir(id);
+
+        return id;
     });
 
    
