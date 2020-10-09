e8363418cd64a15ce216355bc6f537367f93c698
FelixAnnie <contato.anniefelix@gmail.com>
2020-10-09 14:48:32 -0300

    Manual do RH, arquivo provisório

---
+++ manual/arquivoprovisorio.php
@@ -0,0 +1,2 @@
+Edição de dados de funcionários
+Ao acessar os dados de um funcionário é possível fazer a edição desses dados. Na aba "Visão Geral" é possível editar as informações pessoais do funcionário. Existem algumas seções nessa aba, são elas: Informações Pessoais, Endereço, Documentação e Outros. Para fazer a edição de algum dado da página, basta verificar em qual seção ele se encontra e clicar no botão "Editar" da seção. Em seguida será posível alterar os dados, depois de alterados basta clicar no botão "Salvar".
