013d19a77995e48ceb10e33827a3773c89d5dd3f
FelixAnnie <contato.anniefelix@gmail.com>
2020-10-09 17:05:48 -0300

    Manual RH, mais alterações

--- manual/arquivoprovisorio.php
+++ manual/arquivoprovisorio.php
@@ -1,2 +1,16 @@
 Edição de dados de funcionários
-Ao acessar os dados de um funcionário é possível fazer a edição desses dados. Na aba "Visão Geral" é possível editar as informações pessoais do funcionário. Existem algumas seções nessa aba, são elas: Informações Pessoais, Endereço, Documentação e Outros. Para fazer a edição de algum dado da página, basta verificar em qual seção ele se encontra e clicar no botão "Editar" da seção. Em seguida será posível alterar os dados, depois de alterados basta clicar no botão "Salvar".
+Ao acessar os dados de um funcionário é possível fazer a edição desses dados. 
+Na aba "Visão Geral" é possível editar as informações pessoais do funcionário. 
+Existem algumas seções nessa aba, são elas: Informações Pessoais, Endereço, Documentação e Outros. 
+Para fazer a edição de algum dado da página, basta verificar em qual seção ele se encontra e clicar no botão "Editar" da seção. Em seguida será posível alterar os dados, depois de alterados basta clicar no botão "Salvar".
+Na aba "Benefícios" é possível adicionar e editar benefícios.
+Para adicionar um benefício basta clicar no botão "Adicionar" no fim da página. Ao clicar nesse botão é aberta uma caixa para adicionar o benefício.
+Para adicionar um benefício é necessário selecionar o benefício e o seu status e preencher a data de início, data de fim e o valor do benefício. Caso o benefício desejado não se encontre na listagem basta clicar no símbolo de "mais" ao seu lado e na nova caixa que será aberta escrever o nome do benefício e clicar em OK para salvá-lo.
+Na seção de benefícios é possível visualizar a lista de benefícios do funcionário. Nessa lista é exibido o nome do benefício, o status, a data de início, a data de fim e valor. Para editar um benefício basta clicar no botão verde ao seu lado, ao clicar o usuário é redirecionado para uma página onde é possível editar os dados desejados. Para editá-los basta clicar no botão "Editar" e após editar clicar no botão "salvar".
+Para excluir um benefício basta clicar no botão vermelho ao lado dos dados do benefício, o benefício será excluído automaticamente. 
+Na aba "EPI" é possível adicionar e editar EPI's.
+Para adicionar um EPI basta clicar no botão "EPI" no fim da página. Ao clicar nesse botão é aberta uma caixa para adicionar o EPI.
+Para adicionar um EPI é necessário selecionar o EPI e o seu status e preencher a data. Caso o EPI desejado não se encontre na listagem basta clicar no símbolo de "mais" ao seu lado e na nova caixa que será aberta escrever o nome do EPI e clicar em OK para salvá-lo.
+Na seção de EPI é possível visualizar a lista de EPI's do funcionário. Nessa lista é exibido o nome do epi, o status e a data. Para editar um EPI basta clicar no botão verde ao seu lado, ao clicar o usuário é redirecionado para uma página onde é possível ediar os dados desejados. Para editá-los basta clicar no botão "Editar" e após editar clicar no botão "Salvar".
+Para excluir um EPI basta clicar no botão vermelho ao lado dos dados do EPI, o EPI será excluído automaticamente. 
+Na aba "Carga horária" é possível visualizar as informações da carga horária do funcionário, como escala, dias trabalhados, dias de folga, etc.
