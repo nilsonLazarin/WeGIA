0c130b2a6eb3c65114a71e89a40fd264719a3452
Cynthia <59966957+cynthiaAJardim@users.noreply.github.com>
2020-10-22 17:36:15 -0300

    Update cadastroLog.php

--- html/contribuicao/php/cadastroLog.php
+++ html/contribuicao/php/cadastroLog.php
@@ -13,12 +13,12 @@
     $doc = $_POST['doc'];
     $valor_doacao = $_POST['valor_doacao'];
     $data_vencimento = $_POST['dataV'];
-
+    $referencia = $_POST['referencia'];
     //$query->query("INSERT INTO log_contribuicao(id_socio, ip, data, hora, id_sistema, valor_boleto, data_venc_boleto, id_sociotipo)values((SELECT id_socio FROM socio, pessoa WHERE pessoa.id_pessoa=socio.id_pessoa AND pessoa.cpf='$doc'), '$ip_log', '$data', '$horahoje', '$sistema', '$valor_doacao', '$data_vencimento', (SELECT id_sociotipo FROM socio JOIN pessoa ON (pessoa.id_pessoa = socio.id_pessoa)  WHERE pessoa.cpf = '$doc')");
 
     $query->querydados("SELECT id_socio FROM socio, pessoa WHERE pessoa.id_pessoa=socio.id_pessoa AND pessoa.cpf='$doc'");
     $result = $query->result();
     $id_socio = $result['id_socio'];
 
-    $query->query("INSERT INTO log_contribuicao(id_socio, ip, data, hora, id_sistema, valor_boleto, data_venc_boleto, id_sociotipo)values('$id_socio', '$ip_log', '$data', '$horahoje', '$sistema', '$valor_doacao', '$data_vencimento', '$tipoSocio')");
+    $query->query("INSERT INTO log_contribuicao(id_socio, ip, data, hora, id_sistema, valor_boleto, data_venc_boleto, id_sociotipo, referencia, status)values('$id_socio', '$ip_log', '$data', '$horahoje', '$sistema', '$valor_doacao', '$data_vencimento', '$tipoSocio', '$referencia', '')");
 ?>
