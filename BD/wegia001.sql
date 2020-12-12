0cb1e4bb97b7736ee127ac5101a9317be6554cbb
Matheus <matheus.cefetnf@gmail.com>
2020-12-12 04:40:12 -0300

    Geração de boletos semi-finalizada

--- BD/wegia001.sql
+++ BD/wegia001.sql
@@ -915,6 +915,8 @@ CREATE TABLE IF NOT EXISTS `wegia`.`socio` (
   `id_sociostatus` INT NOT NULL,
   `id_sociotipo` INT NOT NULL,
   `email` VARCHAR(256) NULL,
+  `valor_periodo` FLOAT NULL,
+  `data_referencia` DATE NULL,
   PRIMARY KEY (`id_socio`),
   INDEX `fk_socio_socio_status1_idx` (`id_sociostatus` ASC),
   INDEX `fk_socio_pessoa1_idx` (`id_pessoa` ASC),
--- html/socio/sistema/controller/import_conteudo_psociogeracao.php
+++ html/socio/sistema/controller/import_conteudo_psociogeracao.php
@@ -1,4 +1,26 @@
 <section class="body">
+	<style>
+		.detalhes::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+.detalhes::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+.detalhes::-webkit-scrollbar-thumb {
+  background: orange; 
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+.detalhes::-webkit-scrollbar-thumb:hover {
+  background: #e0af26; 
+}
+	</style>
 
 		<!-- start: header -->
 		<header id="header" class="header">
@@ -32,7 +54,7 @@
 
 				<!-- start: page -->
 				<div class="row">
-        <div class="box box-info box-solid socioModal">
+        <div class="box box-info box-solid box-geracaounica">
             <div class="box-header">
               <h3 class="box-title"><i class="far fa-list-alt"></i> Gerar boleto/carnê personalizado</h3>
             </div>
@@ -40,7 +62,52 @@
             <form action="gerar_contribuicao.php" method="POST">
                 <h1>Pesquise o sócio</h1>
                 <input type="text" id="id_pesquisa" name="id_pesquisa" placeholder="Digite o nome do sócio ou o cpf/cnpj" autocomplete="off" size="20" class="form-control">
-                <?php 
+                <div style="display: none" class="row mt-2 configs_unico">
+				<div class="form-group mb-2 col-xs-6 mt-2">
+            <label for="pessoa">O que você deseja gerar?</label>
+                <select class="form-control" name="tipo_geracao" id="tipo_geracao">
+                    <option value="1" selected>Boleto único</option>
+                    <option value="2">Carnê mensal</option>
+                    <option value="3">Carnê bimestral</option>
+                    <option value="4">Carnê trimestral</option>
+					<option value="5">Carnê semestral</option>
+                </select>
+            </div>
+            <div class="form-group mb-2 col-xs-6">
+           
+            <div class="valor">
+            <label for="pessoa">Valor</label>
+            <input type="number" class="form-control" id="valor_u" name="valor" placeholder="Valor único ou por período" required>
+            </div>
+
+
+            </div>
+			<div class="form-group mb-2 col-xs-6">
+           
+		   <div class="data">
+		   <label for="data_vencimento">Data de vencimento (se não for boleto único será a data de vencimento da primeira parcela)</label>
+		   <input type="date" class="form-control" id="data_vencimento" name="data_vencimento" placeholder="Data vencimento" required disabled>
+		   </div>
+
+
+		   </div>
+		   <div class="form-group mb-2 col-xs-6">
+           
+		   <div class="data">
+		   <label for="data_vencimento">Parcelas</label>
+		   <input type="text" class="form-control" id="num_parcelas" name="num_parcelas" placeholder="num. parcelas" required disabled>
+		   </div>
+
+
+
+		   </div>
+		   <div class="col-xs-12 detalhes_unico">
+		   
+
+</div>
+			<div style="display: none" class="col-xs-12 div_btn_gerar"><button type="button" id="btn_geracao_unica" class="btn btn-primary">Confirmar geração</button> <button style="display: none" type="button" id="btn_wpp" class="btn btn-success"><i style="padding: 0; font-size: 20px" class="fab fa-whatsapp"></i></button></div>
+				</div>
+				<?php 
 						$socios = array();
 						$resultado = mysqli_query($conexao,"SELECT *, s.id_socio as socioid FROM socio AS s LEFT JOIN pessoa AS p ON s.id_pessoa = p.id_pessoa LEFT JOIN socio_tipo AS st ON s.id_sociotipo = st.id_sociotipo LEFT JOIN (SELECT id_socio, MAX(data) AS ultima_data_doacao FROM log_contribuicao GROUP BY id_socio) AS lc ON lc.id_socio = s.id_socio");
 						while($registro = mysqli_fetch_assoc($resultado)){
@@ -66,7 +133,7 @@
   			        });
                 </script>
              <div style="margin-top: 2em" class="pull-right mt-2">
-							<button type="submit" class="btn btn-primary">Próximo</button>
+							<button type="button" id="btn_gerar_unico" class="btn btn-primary">Próximo</button>
 						</div>
 					  </form>
 
@@ -84,6 +151,8 @@
 				<div class="alert alert-dark text-center" role="alert">
   					Geração em massa de carnês para sócios mensais, bimestrais, trimestrais e semestrais que não sejam inativos e/ou inadimplentes.<br>
 					Para ser possível a geração, os sócio devem ter o cpf, data de referência e valor por período cadastrados.
+					<br>A geração pode demorar alguns minutos dependendo da quantidade de sócios disponíveis para geração.
+					<br><span style="font-size: 20px; color: orange" id="estimacao_tempo"></span>
 				</div>
             <form action="geracao_auto.php" method="POST">
                 <div class="row">
@@ -100,8 +169,8 @@
 							</div>
 				</div>
 
-				<div style="margin-top: 2em" class="row mt-3 pt-3">
-					  <div class="detalhes col-xs-12">
+				<div style="margin-top: 2em" class="row">
+					  <div style="overflow: auto; max-height: 800px;" class="detalhes col-xs-12">
 					 
 					  </div>
 				</div>
