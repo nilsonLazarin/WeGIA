6e75d7a5093f7a069d3e48a34d8933dbb33ca61d
Matheus Marqui <matheus.cefetnf@gmail.com>
2021-01-22 14:06:38 -0300

    Testando erro no sistema.laje.org, arquivo modificado está dando erro 500 (solicitação)

--- html/socio/sistema/processa_cobrancas_tabela.php
+++ html/socio/sistema/processa_cobrancas_tabela.php
@@ -9,8 +9,8 @@
     }
     $conexao->set_charset("utf8");
 
-    $query = mysqli_query($conexao, "SELECT *, DATE_FORMAT(data_emissao, '%d/%m/%Y') as data_emissao, DATE_FORMAT(data_vencimento, '%d/%m/%Y') as data_vencimento, DATE_FORMAT(data_pagamento, '%d/%m/%Y') as data_pagamento FROM cobrancas c JOIN socio s ON s.id_socio = c.id_socio JOIN pessoa p ON s.id_pessoa = p.id_pessoa");
-    while($resultado = mysqli_fetch_array($query)){
+    $query = mysqli_query($conexao, "SELECT *, DATE_FORMAT(data_emissao, '%d/%m/%Y') as data_emissao, DATE_FORMAT(data_vencimento, '%d/%m/%Y') as data_vencimento, DATE_FORMAT(data_pagamento, '%d/%m/%Y') as data_pagamento FROM cobrancas c JOIN socio s ON s.id_socio = c.id_socio JOIN pessoa p ON s.id_pessoa = p.id_pessoa LIMIT 3000");
+    while($resultado = mysqli_fetch_assoc($query)){
         $dados['data'][] = array_values($resultado);
     }
 
