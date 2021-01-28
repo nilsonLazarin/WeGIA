9cc064482fbd5b3bf61c8143923a56ae7c42272d
Matheus Marqui <matheus.cefetnf@gmail.com>
2021-01-28 14:13:22 -0300

    Tentativa de resolução problema ao aditar sócios (data de referência)

--- html/socio/sistema/processa_edicao_socio.php
+++ html/socio/sistema/processa_edicao_socio.php
@@ -19,7 +19,7 @@
 
     if(!isset($data_referencia) or ($data_referencia == null) or ($data_referencia == "") or empty($data_referencia) or ($data_referencia == "imp")){
         $data_referencia = "null";
-    }else $data_referencia = "$data_referencia";
+    }else $data_referencia = "'$data_referencia'";
 
     if(!isset($valor_periodo) or ($valor_periodo == null) or ($valor_periodo == "") or empty($valor_periodo) or ($valor_periodo == "imp")){
         $valor_periodo = "null";
@@ -27,7 +27,6 @@
 
     $id_pessoa = mysqli_fetch_array(mysqli_query($conexao, "SELECT id_pessoa FROM socio WHERE id_socio = $id_socio"))['id_pessoa'];
     if($resultado = mysqli_query($conexao, "UPDATE `pessoa` SET `cpf` = '$cpf_cnpj', `nome` = '$socio_nome', `telefone` = '$telefone', `data_nascimento` = '$data_nasc', `cep` = '$cep', `estado` = '$estado', `cidade` = '$cidade', `bairro` = '$bairro', `logradouro` = '$rua', `numero_endereco` = '$numero', `complemento` = '$complemento' WHERE id_pessoa = $id_pessoa")){
-        echo("UPDATE `pessoa` SET `cpf` = '$cpf_cnpj', `nome` = '$socio_nome', `telefone` = '$telefone', `data_nascimento` = '$data_nasc', `cep` = '$cep', `estado` = '$estado', `cidade` = '$cidade', `bairro` = '$bairro', `logradouro` = '$rua', `numero_endereco` = '$numero', `complemento` = '$complemento' WHERE id_pessoa = $id_pessoa");
         switch($pessoa){
             case "juridica": 
             if($contribuinte == "mensal"){
@@ -64,8 +63,7 @@
                 $id_sociotipo = 4;
             }  break;
         }
-        if($resultado = mysqli_query($conexao, "UPDATE `socio` SET `id_sociostatus`= '$status', `id_sociotipo` = '$id_sociotipo', `email` = '$email', `data_referencia` = '$data_referencia', `valor_periodo` = $valor_periodo WHERE id_socio = $id_socio")){
-            echo("UPDATE `socio` SET `id_sociostatus`= '$status', `id_sociotipo` = '$id_sociotipo', `email` = '$email', `data_referencia` = '$data_referencia', `valor_periodo` = $valor_periodo WHERE id_socio = $id_socio");
+        if($resultado = mysqli_query($conexao, "UPDATE `socio` SET `id_sociostatus`= '$status', `id_sociotipo` = '$id_sociotipo', `email` = '$email', `data_referencia` = $data_referencia, `valor_periodo` = $valor_periodo WHERE id_socio = $id_socio")){
             $cadastrado = true;
         }
         
