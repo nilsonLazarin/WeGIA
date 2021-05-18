b1f561c13265236a104ef85348b7889730bdee45
Antonio Henrique <hgfantonio@gmail.com>
2021-05-18 17:00:32 -0300

    Resolvido bug do menu de usuário

--- html/profile_funcionario.php
+++ html/profile_funcionario.php
@@ -113,13 +113,6 @@ $dependente = json_encode($dependente);
   <link rel="stylesheet" href="../assets/vendor/magnific-popup/magnific-popup.css" />
   <link rel="stylesheet" href="../assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
   <link rel="icon" href="<?php display_campo("Logo", 'file'); ?>" type="image/x-icon" id="logo-icon">
-  <script src="../assets/vendor/jquery/jquery.min.js"></script>
-  <script src="../assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.js"></script>
-  <script src="../assets/vendor/nanoscroller/nanoscroller.js"></script>
-  <script src="../assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-  <script src="../assets/vendor/magnific-popup/magnific-popup.js"></script>
-  <script src="../assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
   <!-- Theme CSS -->
   <link rel="stylesheet" href="../assets/stylesheets/theme.css" />
   <!-- Skin CSS -->
