e8bcb328e1ddd9f555fc5eec40f701af8e1c1565
Antonio Henrique <hgfantonio@gmail.com>
2021-05-19 15:48:23 -0300

    Correção de texto em cadastrar dependente

--- html/profile_funcionario.php
+++ html/profile_funcionario.php
@@ -1904,7 +1904,7 @@ $dependente = json_encode($dependente);
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header" style="display: flex;justify-content: space-between;">
-                            <h5 class="modal-title" id="exampleModalLabel">Adicionar Documento</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Adicionar Dependente</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                               <span aria-hidden="true">&times;</span>
                             </button>
