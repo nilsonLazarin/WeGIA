b689bca56fe62a15d6da492aea2a57d01188795e
emanuellebiscacio <emanuellebiscacio@gmail.com>
2021-07-19 12:02:42 -0300

    tarefa senha aleatória

--- controle/FuncionarioControle.php
+++ controle/FuncionarioControle.php
@@ -84,26 +84,10 @@ class FuncionarioControle
         return '';
     }
 
-    function geraChave($cpf, $tamanho = 8, $maiusculas = true, $numeros = true, $simbolos = false)
+    function geraChave()
     {
-        $lmin = 'abcdefghijklmnopqrstuvwxyz';
-        $lmai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $num = '1234567890';
-        $simb = '!@#$%*-';
-        $retorno = '';
-        $caracteres = '';
-        $caracteres .= $lmin;
-        if ($maiusculas) $caracteres .= $lmai;
-        if ($numeros) $caracteres .= $num;
-        if ($simbolos) $caracteres .= $simb;
-        $len = strlen($caracteres);
-        for ($n = 1; $n <= $tamanho; $n++) {
-            $rand = mt_rand(1, $len);
-            $retorno .= $caracteres[$rand-1];
-        }
-        
-        $retorno=hash('sha256', $retorno);
-        return $retorno;
+           
+        return '';
     }
 
     public function verificarHorario(){
