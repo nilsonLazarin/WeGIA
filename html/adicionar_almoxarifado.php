b1e87480a3e7ecd8710012ab75db7a449914d43f
Prof Nilson Mori <32855001+nilsonmori@users.noreply.github.com>
2022-04-05 11:15:07 -0300

    Update adicionar_almoxarifado.php

--- html/adicionar_almoxarifado.php
+++ html/adicionar_almoxarifado.php
@@ -211,5 +211,8 @@
 			</div>
 		</aside>
 	</section>
+	<div align="right">
+	<iframe src="https://www.wegia.org/software/footer/estoque.html" width="200" height="60" style="border:none;"></iframe>
+	</div>
 </body>
 </html>
