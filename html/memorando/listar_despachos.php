6550271a9066199bf5945b4ad92b0782c69e4672
Prof Nilson Mori <32855001+nilsonmori@users.noreply.github.com>
2022-05-14 01:52:16 -0300

    Update listar_despachos.php

--- html/memorando/listar_despachos.php
+++ html/memorando/listar_despachos.php
@@ -846,5 +846,8 @@ $id?>
 			});
 		});       	
 	</script>
+	<div align="right">
+	<iframe src="https://www.wegia.org/software/footer/memorando.html" width="200" height="60" style="border:none;"></iframe>
+	</div>
 	</body>
 </html>
