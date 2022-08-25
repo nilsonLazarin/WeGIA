c085cd5db7c9f8c4b74f0fb3b66f828265764a11
pedrohenriqueabib <pedrohenriqueabib@gmail.com>
2022-08-25 15:53:46 -0300

    manutencao5

--- html/pet/profile_pet.php
+++ html/pet/profile_pet.php
@@ -266,25 +266,24 @@
                 <div class="panel-body">
                   <div class="thumb-info mb-md">
                     <?php
-                    
-                      $conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+                      
+                      /*$conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);*/
                       $id_pessoa = $_SESSION['id_pessoa'];
                       $donoimagem = $_GET['id_pet'];
-                      $resultado = mysqli_query($conexao, "SELECT p.id_pet_foto AS id_foto, pf.arquivo_foto_pet AS 'imagem' FROM pet p, pet_foto pf WHERE p.id_pet_foto=pf.id_pet_foto and p.id_pet=$donoimagem");
-                      if($resultado){
-                        $petImagem = mysqli_fetch_array($resultado);            
-                        if (isset($_SESSION['id_pessoa']) and !empty($_SESSION['id_pessoa'])) 
+                      require "./foto.php";
+                      /*$resultado = mysqli_query($conexao, "SELECT p.id_pet_foto AS id_foto, pf.arquivo_foto_pet AS 'imagem' FROM pet p, pet_foto pf WHERE p.id_pet_foto=pf.id_pet_foto and p.id_pet=$donoimagem");
+                      $petImagem = mysqli_fetch_array($resultado);*/
+                      if (isset($_SESSION['id_pessoa']) and !empty($_SESSION['id_pessoa'])) 
+                      {
+                        $foto = $petImagem['imagem'];
+                        $id_foto = $petImagem['id_foto'];
+                        if ($foto != null and $foto != "")
                         {
-                          $foto = $petImagem['imagem'];
-                          $id_foto = $petImagem['id_foto'];
-                          if ($foto != null and $foto != "")
-                          {
-                            $foto = 'data:image;base64,' . $foto;
-                          }
-                          else 
-                          {
-                            $foto = WWW . "img/semfoto.png";
-                          }
+                          $foto = 'data:image;base64,' . $foto;
+                        }
+                        else 
+                        {
+                          $foto = WWW . "img/semfoto.png";
                         }
                       }
                       echo "<img src='$foto' id='imagem' class='rounded img-responsive' alt='John Doe'>";
