d8bbf68e61c530b45cdb85e5529f35d83c81fd50
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-03-22 07:54:52 -0300

    Atribuição de uma action para o formulário de cadastro de sócios.

--- html/relatorio.php
+++ html/relatorio.php
@@ -256,11 +256,11 @@ require_once "personalizacao_display.php";
 										<option value="">Todas as Opções</option>
 										<?php
 										$pdo = Conexao::connect();
-										$res = $pdo->query("SELECT DISTINCT p.* FROM pessoa p JOIN funcionario f ON (f.id_pessoa = p.id_pessoa) JOIN almoxarife a ON (f.id_funcionario =a.id_funcionario);"); //modificar para pegar apenas almoxarifes
+										$res = $pdo->query("SELECT DISTINCT p.* FROM pessoa p JOIN funcionario f ON (f.id_pessoa = p.id_pessoa) JOIN almoxarife a ON (f.id_funcionario =a.id_funcionario) ORDER BY p.nome ASC;");
 										$resp = $res->fetchAll(PDO::FETCH_ASSOC);
 										foreach ($resp as $value) {
 											echo ('
-												<option value="' . $value['id_pessoa'] . '">' . $value['nome'] . '</option>
+												<option value="' . $value['id_pessoa'] . '">' . $value['nome'] . ' ' . $value['sobrenome'] . '</option>
 												');
 										}
 										?>
