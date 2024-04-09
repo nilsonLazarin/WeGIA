f191bfc7980b0cf79d4fa5577ccd5647d413859f
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-04-09 08:59:16 -0300

    Conserto de SQL injection no arquivo dao adicionar_status_atendido.php

--- dao/adicionar_status_atendido.php
+++ dao/adicionar_status_atendido.php
@@ -3,6 +3,12 @@
 	$pdo = Conexao::connect();
 	$status = $_POST["status"];
 
-	$sql = "INSERT into atendido_status(status) values('" .$status ."')";
-	$pdo->query($sql);
-?>
+	$sql = "INSERT into atendido_status(status) values(:status)";
+
+	try{
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':status', $status);
+		$stmt->execute();
+	}catch(PDOException $e){
+		$e->getMessage();
+	}
