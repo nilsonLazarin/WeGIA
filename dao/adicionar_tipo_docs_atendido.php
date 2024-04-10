d30db3c23e0d8da4ee5ec57b8547bcf10cc11edf
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-04-10 07:15:19 -0300

    Conserto de SQL injection no arquivo dao adicionar_tipo_docs_atendido.php

--- dao/adicionar_tipo_docs_atendido.php
+++ dao/adicionar_tipo_docs_atendido.php
@@ -1,8 +1,14 @@
 <?php
-	require_once'Conexao.php';
-	$pdo = Conexao::connect();
-	$descricao = $_POST["tipo"];
+require_once 'Conexao.php';
+$pdo = Conexao::connect();
+$descricao = $_POST["tipo"];
 
-	$sql = "INSERT into atendido_docs_atendidos(descricao) values('" .$descricao ."')";
-	$pdo->query($sql);
-?>
+$sql = "INSERT into atendido_docs_atendidos(descricao) values(:descricao)";
+
+try {
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(':descricao', $descricao);
+	$stmt->execute();
+} catch (PDOException $e) {
+	$e->getMessage();
+}
