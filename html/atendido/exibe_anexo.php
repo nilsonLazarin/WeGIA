9e333a15622f84bb7219972328b6e0c022b6fefd
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-05-21 11:32:17 -0300

    Refatoração do arquivo html/atendido/exibe_anexo.php

--- html/atendido/exibe_anexo.php
+++ html/atendido/exibe_anexo.php
@@ -1,33 +1,41 @@
 <?php
 $config_path = "config.php";
-if(file_exists($config_path)){
+if (file_exists($config_path)) {
     require_once($config_path);
-}else{
-    while(true){
+} else {
+    while (true) {
         $config_path = "../" . $config_path;
-        if(file_exists($config_path)) break;
+        if (file_exists($config_path)) break;
     }
     require_once($config_path);
 }
 
 session_start();
 
-if(!isset($_SESSION['usuario'])){
-	header ("Location: ".WWW."index.php");
+if (!isset($_SESSION['usuario'])) {
+    header("Location: " . WWW . "index.php");
 }
 
-require_once ROOT."/controle/Atendido_ocorrenciaControle.php";
+require_once ROOT . "/controle/Atendido_ocorrenciaControle.php";
 
 $id_anexo = $_GET['idatendido_ocorrencias'];
-$extensao = $_GET['extensao'];
-$nome = $_GET['nome'];
+$extensao = trim($_GET['extensao']);
+$nome = trim($_GET['nome']);
+
+if (!$id_anexo || !is_numeric($id_anexo)) {
+    http_response_code(400);
+    exit("Erro ao exibir anexo, o id fornecido não é um número válido para essa operação");
+}
+
+if (!$extensao || empty($extensao) || !$nome || empty($nome)) {
+    http_response_code(400);
+    exit("Erro ao exibir anexo, a combinação do nome com o formato do arquivo não é válida.");
+}
 
 $AnexoControle = new Atendido_ocorrenciaControle;
 $AnexoControle->listarAnexo($id_anexo);
 
-header('Content-Type: application/force-download');
+header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="' . $nome . '.' . $extensao . '"');
 
-/*Header('Content-Disposition: attachment; filename="'.$nome.'.'.$extensao);*/
 echo $_SESSION['arq'][0]['anexo'];
-?>
