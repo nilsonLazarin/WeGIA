e5ce3e83f4ee401bc02bdfbaf525053f7733225f
nicolly015 <nicollyboy01@gmail.com>
2024-05-24 17:05:44 -0300

    Resolvendo [#216]

--- html/socio/sistema/controller/controla_xlsx_cobranca.php
+++ html/socio/sistema/controller/controla_xlsx_cobranca.php
@@ -1,19 +1,32 @@
 <?php
-    $r = array(
-        "resultado"=> false,
-        "url"=> null
-    );
-    if(!is_dir("../tabelas/")){
-        if(mkdir("../tabelas")){
-            mkdir("../tabelas/cobrancas/");
-        }
-    }
-    if(!empty($_FILES['arquivo']['name'])){
-        if(move_uploaded_file($_FILES['arquivo']['tmp_name'], "../tabelas/cobrancas/".$_FILES['arquivo']['name'])){
-            $r['resultado'] = true;
-            $r['url'] = "./tabelas/cobrancas/".$_FILES['arquivo']['name'];
-        }
-    }
+$r = array(
+    "resultado"=> false,
+    "url"=> null
+);
 
-    echo(json_encode($r))
+if (!is_dir("../tabelas/")) {
+    if (mkdir("../tabelas")) {
+        mkdir("../tabelas/cobrancas/");
+    }
+}
+
+if (!empty($_FILES['arquivo']['name'])) {
+    $bloqueados = array('php', 'exe', 'sh', 'bat', 'js', 'html', 'htm');
+    $extensao = strtolower(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
+
+    if (!in_array($extensao, $bloqueados)) {
+        $file_name = preg_replace("/[^a-zA-Z0-9\._-]/", "_", basename($_FILES['arquivo']['name']));
+
+        if (move_uploaded_file($_FILES['arquivo']['tmp_name'], "../tabelas/cobrancas/".$file_name)) {
+            $r['resultado'] = true;
+            $r['url'] = "./tabelas/cobrancas/".$file_name;
+        } else {
+            $r['url'] = "Erro ao mover o arquivo.";
+        }
+    } else {
+        $r['url'] = "Tipo de arquivo não permitido.";
+    }
+}
+
+echo json_encode($r);
 ?>
