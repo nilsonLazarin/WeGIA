485d3951e3274ced2747b8c45618e73a45869419
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-05-24 10:36:34 -0300

    Refatoração do arquivo dao/adicionar_tipo_docs_atendido.php

--- dao/adicionar_tipo_docs_atendido.php
+++ dao/adicionar_tipo_docs_atendido.php
@@ -1,14 +1,19 @@
 <?php
 require_once 'Conexao.php';
-$pdo = Conexao::connect();
-$descricao = $_POST["tipo"];
 
-$sql = "INSERT into atendido_docs_atendidos(descricao) values(:descricao)";
+$descricao = trim($_POST["tipo"]);
+
+if(!$descricao || empty($descricao)){
+	http_response_code(400);
+	exit('Erro, a descrição de um novo tipo de documentação não pode ser vazia.');
+}
 
 try {
+	$sql = "INSERT into atendido_docs_atendidos(descricao) values(:descricao)";
+	$pdo = Conexao::connect();
 	$stmt = $pdo->prepare($sql);
 	$stmt->bindParam(':descricao', $descricao);
 	$stmt->execute();
 } catch (PDOException $e) {
-	$e->getMessage();
+	echo 'Erro ao inserir um novo tipo de documentação no banco de dados: '.$e->getMessage();
 }
