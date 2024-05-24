91b33480f14c94001894f82b22d700604799ebb1
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-05-24 08:29:02 -0300

    Refatoração do arquivo dao/adicionar_documento.php

--- dao/adicionar_documento.php
+++ dao/adicionar_documento.php
@@ -1,14 +1,19 @@
 <?php
 require_once 'Conexao.php';
-$pdo = Conexao::connect();
-$nomeDocFuncional = $_POST["nome_docfuncional"];
 
-$sql = "INSERT into funcionario_docfuncional(nome_docfuncional) values(:nomeDocFuncional)";
+$nomeDocFuncional = trim($_POST["nome_docfuncional"]);
+
+if(!$nomeDocFuncional || empty($nomeDocFuncional)){
+	http_response_code(400);
+	exit('Erro, o nome de um novo tipo de documento não pode ser vazio.');
+}
 
 try {
+	$sql = "INSERT into funcionario_docfuncional(nome_docfuncional) values(:nomeDocFuncional)";
+	$pdo = Conexao::connect();
 	$stmt = $pdo->prepare($sql);
 	$stmt->bindParam(':nomeDocFuncional', $nomeDocFuncional);
 	$stmt->execute();
 } catch (PDOException $e) {
-	$e->getMessage();
+	echo 'Erro ao inserir um novo tipo de documento no banco de dados: '.$e->getMessage();
 }
