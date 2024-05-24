eb3e2e050b66ad11b5d15151c1843c05b62a39b5
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-05-24 09:11:50 -0300

    Refatoração do arquivo adicionar_situacao.php

--- dao/adicionar_situacao.php
+++ dao/adicionar_situacao.php
@@ -1,14 +1,19 @@
 <?php
 require_once 'Conexao.php';
-$pdo = Conexao::connect();
-$situacao = $_POST["situacao"];
 
-$sql = "INSERT into situacao(situacoes) values(:situacao)";
+$situacao = trim($_POST["situacao"]);
+
+if(!$situacao || empty($situacao)){
+	http_response_code(400);
+	exit('Erro, a descrição de uma nova situação não pode ser vazia.');
+}
 
 try {
+	$sql = "INSERT into situacao(situacoes) values(:situacao)";
+	$pdo = Conexao::connect();
 	$stmt = $pdo->prepare($sql);
 	$stmt->bindParam(':situacao', $situacao);
 	$stmt->execute();
 } catch (PDOException $e) {
-	$e->getMessage();
+	echo 'Erro ao inserir uma nova situação no banco de dados: '.$e->getMessage();
 }
