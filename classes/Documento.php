5f80300ca424cd962e80cc47188b12b774f09a83
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-06-06 07:33:10 -0300

    Resolução da issue #556

--- classes/Documento.php
+++ classes/Documento.php
@@ -1,57 +1,71 @@
-<?php 
-	class Documento
+<?php
+class Documento
+{
+	private $imagem;
+	private $extensao;
+	private $descricao;
+	private $idPessoa;
+	private $idDocumento;
+	public function __construct($idPessoa, $imagem, $extensao, $descricao)
 	{
-		private $imagem;
-		private $extensao;
-		private $descricao;
-		private $idPessoa;
-		private $idDocumento;
-		public function __construct($idPessoa,$imagem,$extensao,$descricao)
-		{
-			$this->idPessoa=$idPessoa;
-			$this->imagem=$imagem;
-			$this->descricao=$descricao;
-			$this->extensao=$extensao;
+		$this->setIdPessoa($idPessoa);
+		$this->setImagem($imagem);
+		$this->setExtensao($extensao);
+		$this->setDescricao($descricao);
+	}
+	public function getImagem()
+	{
+		return $this->imagem;
+	}
+	public function getExtensao()
+	{
+		return $this->extensao;
+	}
+	public function getDescricao()
+	{
+		return $this->descricao;
+	}
+	public function getIdPessoa()
+	{
+		return $this->idPessoa;
+	}
+	public function getIdDocumento()
+	{
+		return $this->idDocumento;
+	}
+	public function setImagem(string $imagem)
+	{
+		if (empty($imagem)) {
+			throw new InvalidArgumentException('A imagem de um documento não pode ser vazia.');
 		}
-		public function getImagem()
-		{
-			return $this->imagem;
+		$this->imagem = $imagem;
+	}
+	public function setExtensao(string $extensao)
+	{
+		if (empty($extensao)) {
+			throw new InvalidArgumentException('A extensão de um documento não pode ser vazia.');
 		}
-		public function getExtensao()
-		{
-			return $this->extensao;
+		$this->extensao = $extensao;
+	}
+	public function setDescricao(string $descricao)
+	{
+		if (empty($descricao)) {
+			throw new InvalidArgumentException('A descrição de um documento não pode ser vazia.');
 		}
-		public function getDescricao()
-		{
-			return $this->descricao;
+		$this->descricao = $descricao;
+	}
+	public function setIdPessoa(int $idPessoa)
+	{
+		if ($idPessoa < 1) {
+			throw new InvalidArgumentException('O id de uma pessoa não pode ser menor que 1.');
 		}
-		public function getIdPessoa()
-		{
-			return $this->idPessoa;
+		$this->idPessoa = $idPessoa;
+	}
+	public function setIdDocumento(int $idDocumento)
+	{
+		if ($idDocumento < 1) {
+			throw new InvalidArgumentException('O id de um documento não pode ser menor que 1.');
 		}
-		public function getIdDocumento()
-		{
-			return $this->idDocumento;
-		}
-		public function setImagem($imagem)
-		{
-			$this->imagem=$imagem;
-		}
-		public function setExtensao($extensao)
-		{
-			$this->extensao=$extensao;
-		}
-		public function setDescricao($descricao)
-		{
-			$this->descricao=$descricao;
-		}
-		public function setIdPessoa($idPessoa)
-		{
-			$this->idPessoa=$idPessoa;
-		}
-		public function setIdDocumento($idDocumento)
-		{
-			$this->idDocumento=$idDocumento;
-		}
-	} 
-?>
+		$this->idDocumento = $idDocumento;
+	}
+}
