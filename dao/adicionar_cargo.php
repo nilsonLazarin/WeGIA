317a1da17c98371c513a9b4fd0ab2df2e1038498
nicolly015 <nicollyboy01@gmail.com>
2024-06-12 15:50:19 -0300

    Resolvendo [#209]

--- dao/adicionar_cargo.php
+++ dao/adicionar_cargo.php
@@ -1,7 +1,7 @@
 <?php
 	require_once'Conexao.php';
 	$pdo = Conexao::connect();
-	$cargo = $_POST["cargo"];
+	$cargo = mysqli_real_escape_string($conexao, $_POST['cargo']);
 
 	$sql = "INSERT into cargo(cargo) values('" .$cargo ."')";
 	$pdo->query($sql);
