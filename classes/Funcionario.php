4f589c154d463e428d7148e489fc852644163002
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-06-14 10:36:32 -0300

    Resolução da issue #550

--- classes/Funcionario.php
+++ classes/Funcionario.php
@@ -3,12 +3,10 @@ require_once 'Pessoa.php';
 
 class Funcionario extends Pessoa
 {
-
     private $id_funcionario;
     private $id_pessoa;
     private $id_cargo;
     private $id_situacao;
-    //private $vale_transporte;
     private $data_admissao;
     private $pis;
     private $ctps;
@@ -18,8 +16,6 @@ class Funcionario extends Pessoa
     private $secao;
     private $certificado_reservista_numero;
     private $certificado_reservista_serie;
-    //private $usa_vtp;
-    //private $cesta_basica;
 
     public function getId_funcionario()
     {
@@ -34,16 +30,6 @@ class Funcionario extends Pessoa
     {
         return $this->id_pessoa;
     }
-
-    /*public function getId_quadro_horario()
-    {
-        return $this->id_quadro_horario;
-    }*/
-
-    /*public function getVale_transporte()
-    {
-        return $this->vale_transporte;
-    }*/
 
     public function getData_admissao()
     {
@@ -90,26 +76,11 @@ class Funcionario extends Pessoa
         return $this->certificado_reservista_serie;
     }
 
-    /*public function getUsa_vtp()
-    {
-        return $this->usa_vtp;
-    }
-
-    public function getCesta_basica()
-    {
-        return $this->cesta_basica;
-    }*/
-
     public function getId_situacao()
     {
         return $this->id_situacao;
     }
-    /*
-    public function getQuadro_horario()
-    {
-        return $this->quadro_horario;
-    }
-    */
+
     public function setId_funcionario($id_funcionario)
     {
         $this->id_funcionario = $id_funcionario;
@@ -120,16 +91,6 @@ class Funcionario extends Pessoa
         $this->id_pessoa = $id_pessoa;
     }
 
-    /*
-    public function setId_quadro_horario($id_quadro_horario)
-    {
-        $this->id_quadro_horario = $id_quadro_horario;
-    }
-    
-    public function setVale_transporte($vale_transporte)
-    {
-        $this->vale_transporte = $vale_transporte;
-    }*/
     public function setId_cargo($id_cargo)
     {
         $this->id_cargo = $id_cargo;
@@ -180,27 +141,14 @@ class Funcionario extends Pessoa
         $this->certificado_reservista_serie = $certificado_reservista_serie;
     }
 
-    /*public function setUsa_vtp($usa_vtp)
-    {
-        $this->usa_vtp = $usa_vtp;
-    }
-
-    public function setCesta_basica($cesta_basica)
-    {
-        $this->cesta_basica = $cesta_basica;
-    }*/
-
     public function setId_situacao($id_situacao)
     {
         $this->id_situacao = $id_situacao;
     }
 
-    /*public function setQuadro_horario($quadro_horario)
-    {
-        $this->quadro_horario = $quadro_horario;
-    }*/
-
-    //Data de nascimento máxima
+    /**
+     * Retorna a data máxima de nascimento para o cadastro de um novo funcionário no sistema.
+     */
     static public function getDataNascimentoMaxima()
     {
         $idadeMinima = 16;
@@ -208,7 +156,11 @@ class Funcionario extends Pessoa
         return $data;
     }
 
-    static public function getDataNascimentoMinima(){
+    /**
+     * Retorna a data mínima de nascimento para o cadastro de um novo funcionário no sistema.
+     */
+    static public function getDataNascimentoMinima()
+    {
         $idadeMaxima = 100;
         $data = date('Y-m-d', strtotime("-$idadeMaxima years"));
         return $data;
