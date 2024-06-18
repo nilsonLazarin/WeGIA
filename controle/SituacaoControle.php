8bf7e536f68fbce72fb4d79f2d81cdea65c0c844
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-06-18 11:21:21 -0300

    Resolução da issue #468

--- controle/SituacaoControle.php
+++ controle/SituacaoControle.php
@@ -3,12 +3,12 @@ include_once '../classes/Situacao.php';
 include_once '../dao/SituacaoDAO.php';
 class SituacaoControle
 {
-    
     public function verificar(){
-        extract($_REQUEST);
-        
+        //extract($_REQUEST);
+        $situacoes = trim($_REQUEST['situacoes']);
+
         if((!isset($situacoes)) || (empty($situacoes))){
-            $msg .= "Descricao da Situacão não informada. Por favor, informe uma descrição!";
+            $msg = "Descricao da Situacão não informada. Por favor, informe uma descrição!";
             header('Location: ../html/situacao.html?msg='.$msg);
         }else{
             $situacao = new Situacao($situacoes);
@@ -16,11 +16,23 @@ class SituacaoControle
         return $situacao;
     }
     public function listarTodos(){
-        extract($_REQUEST);
+        $situacoes = trim($_REQUEST['situacoes']);
+        $nextPage = trim($_REQUEST['nextPage']);
+
+        if(!$situacoes || empty($situacoes)){
+            http_response_code(400);
+            exit('A descrição da situação não pode ser vazia.');
+        }
+
+        if(!filter_var($nextPage, FILTER_VALIDATE_URL)){
+            http_response_code(400);
+            exit('Erro, a URL informada para a próxima página não é válida.');
+        }
+
         $situacaoDAO= new SituacaoDAO();
         $situacaos = $situacaoDAO->listarTodos();
         session_start();
-        $_SESSION['situacao']=$situacoes;
+        $_SESSION['situacao']=$situacaos;
         header('Location: '.$nextPage);
     }
     public function incluir(){
@@ -34,12 +46,18 @@ class SituacaoControle
             $_SESSION['link']="../html/adicionar_situacao.php";
             header("Location: ../html/adicionar_situacao.php");
         } catch (PDOException $e){
-            $msg= "Não foi possível registrar o situacao"."<br>".$e->getMessage();
+            $msg= "Não foi possível registrar a situacao"."<br>".$e->getMessage();
             echo $msg;
         }
     }
     public function excluir(){
-        extract($_REQUEST);
+        $id_situacao = trim($_REQUEST['id_situacao']);
+
+        if(!$id_situacao || !is_numeric($id_situacao) || $id_situacao < 1){
+            http_response_code(400);
+            exit('O id de uma situação deve ser um número inteiro maior ou igual a 1.');
+        }
+
         try {
             $situacaoDAO=new SituacaoDAO();
             $situacaoDAO->excluir($id_situacao);
