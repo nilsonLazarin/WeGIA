81e2c627365e7acd3cb09e627f1f3ab4fc4831d6
nicolly015 <nicollyboy01@gmail.com>
2024-06-19 11:23:55 -0300

    Resolvendo conflito entre as branchs

--- dao/adicionar_cargo.php
+++ dao/adicionar_cargo.php
@@ -1,8 +1,20 @@
 <?php
 	require_once'Conexao.php';
-	$pdo = Conexao::connect();
-	$cargo = mysqli_real_escape_string($conexao, $_POST['cargo']);
 
-	$sql = "INSERT into cargo(cargo) values('" .$cargo ."')";
-	$pdo->query($sql);
+	$cargo = trim($_POST['cargo']);
+
+	if(!$cargo || empty($cargo)){
+		http_response_code(400);
+		exit('Erro, a descrição fornecida para o cargo não pode ser vazia.');
+	}
+	
+	try {
+		$sql = "INSERT into cargo(cargo) values(:cargo)";
+		$pdo = Conexao::connect();
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':cargo', $cargo);
+		$stmt->execute();
+	} catch (PDOException $e) {
+		echo 'Erro ao adicionar novo cargo: '.$e->getMessage();
+	}
 ?>
