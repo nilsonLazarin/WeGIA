ecfc991220a0399aea02778813a51b4657de83a7
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-07-01 08:10:22 -0300

    Criação da classe de controle StatusMemorandoControle e seu respectivo método getPorId()

--- controle/memorando/StatusMemorandoControle.php
+++ controle/memorando/StatusMemorandoControle.php
@@ -1,1 +1,26 @@
 <?php
+
+require_once '../../classes/memorando/StatusMemorando.php';
+require_once '../../dao/memorando/StatusMemorandoDAO.php';
+
+ class StatusMemorandoControle{
+    public function getPorId(int $id){
+        try{
+            if($id < 1){
+                throw new InvalidArgumentException('O id de um memorando não pode ser menor que 1.');
+            }
+
+            $statusMemorandoDAO = new StatusMemorandoDAO();
+            $resultado = $statusMemorandoDAO->getPorId($id);
+
+            if(!$resultado){
+                return null;
+            }
+
+            $statusMemorando = new StatusMemorando($resultado['status_atual'], $resultado['id_status_memorando']);
+            return $statusMemorando();
+        }catch(Exception $e){
+            echo 'Erro ao buscar o status do memorando: '.$e->getMessage();
+        }
+    }
+ }
---
+++ dao/memorando/StatusMemorandoDAO.php
@@ -0,0 +1,6 @@
+<?php
+class StatusMemorandoDAO{
+     public function getPorId(){
+        
+     }
+}
