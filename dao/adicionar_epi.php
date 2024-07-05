1606b1f6223c5353a45e89656b8e2e9709038801
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-07-05 07:45:13 -0300

    Resolução da issue #442

--- dao/adicionar_epi.php
+++ dao/adicionar_epi.php
@@ -1,8 +1,19 @@
 <?php
-	require_once'Conexao.php';
-	$pdo = Conexao::connect();
-	$descricao_epi = $_POST["descricao_epi"];
+require_once 'Conexao.php';
 
-	$sql = "INSERT into epi(descricao_epi) values('" .$descricao_epi ."')";
-	$pdo->query($sql);
-?>
+$descricao_epi = trim($_POST["descricao_epi"]);
+
+if (!$descricao_epi || empty($descricao_epi)) {
+	http_response_code(400);
+	exit('Erro ao adicionar EPI: A descrição de um EPI não pode ser vazia.');
+}
+
+try {
+	$pdo = Conexao::connect();
+	$sql = "INSERT into epi(descricao_epi) values(:descricaoEpi)";
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(':descricaoEpi', $descricao_epi);
+	$stmt->execute();
+} catch (PDOException $e) {
+	echo 'Erro ao adicionar EPI: '.$e->getMessage();
+}
