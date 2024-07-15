0b0f1887bed4b5796dbcbbf2c1b9c9938fa5b427
dev <jgmppacheco@gmail.com>
2024-07-15 12:00:10 -0300

    versao incial do boleto

---
+++ html/contribuicao/doacao/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="boleto_pagar_me.php">Boleto simples</a>
+    <a href="carne.php">Carnê</a>
+</body>
+</html>
