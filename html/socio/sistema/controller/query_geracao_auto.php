3d0b72768f322814e9e33c68bd0f429838f8a977
nicolly015 <nicollyboy01@gmail.com>
2024-07-26 10:14:40 -0300

    Resolvendo issue [#194] e erro 500

--- html/socio/sistema/controller/query_geracao_auto.php
+++ html/socio/sistema/controller/query_geracao_auto.php
@@ -1,25 +1,40 @@
 <?php
     require("../../conexao.php");
-    if(!isset($_POST) or empty($_POST)){
-        $data = file_get_contents( "php://input" );
-        $data = json_decode( $data, true );
-        $_POST = $data;
-    }else if(is_string($_POST)){
-        $_POST = json_decode($_POST, true);
-    }
-    $conexao->set_charset("utf8");
-    extract($_REQUEST);
-    $query = mysqli_query($conexao, $query);
-    while($resultado = mysqli_fetch_assoc($query)){
-        $dados[] = $resultado;
+
+    function cleanInput($data) {
+        return htmlspecialchars(strip_tags(trim($data)), ENT_QUOTES, 'UTF-8');
     }
 
-    // array_walk_recursive($dados, function (&$val) {
-    //     if (is_string($val)) {
-    //         $val = mb_convert_encoding($val, 'UTF-8', 'UTF-8');
-    //     }
-    // });
-    if(isset($dados)){
-        echo json_encode($dados);
-    }else echo json_encode(false);
+    if (!isset($_POST) or empty($_POST)) {
+        $data = file_get_contents("php://input");
+        $data = json_decode($data, true);
+        if (is_array($data)) {
+            $_POST = $data;
+        }
+    } else if (is_string($_POST)) {
+        $_POST = json_decode($_POST, true);
+    }
+
+    $conexao->set_charset("utf8");
+
+    extract($_REQUEST);
+
+    if (isset($_POST['query']) && !empty($_POST['query'])) {
+        $query = cleanInput($_POST['query']);
+        $query = mysqli_query($conexao, $query);
+        while($resultado = mysqli_fetch_assoc($query)){
+            $dados[] = $resultado;
+        }
+
+        // array_walk_recursive($dados, function (&$val) {
+        //     if (is_string($val)) {
+        //         $val = mb_convert_encoding($val, 'UTF-8', 'UTF-8');
+        //     }
+        // });
+        if(isset($dados)){
+            echo json_encode($dados);
+        }else echo json_encode(false);
+    } else {
+        echo json_encode(['error' => 'Query nao fornecida ou esta vazia.']);
+    }
 ?>
