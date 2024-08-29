25bf80b3625577757f8a748d99c2a33003cb835f
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-08-29 11:13:53 -0300

    Importação das páginas getaway_pagamento, meio_pagamento e regras_pagamento do backup do Davi

--- html/menu.php
+++ html/menu.php
@@ -404,6 +404,35 @@
 						</ul>
 					</li>
 
+					<!--contribuiçao-->
+					<li class="nav-parent nav-active visivel">
+						<a>
+							<i class="fa-solid fa-building" aria-hidden="true"></i>
+							<span>Contribuição</span>
+						</a>
+						<ul class="nav nav-children">
+						<li >
+								<a href="<?= WWW ?>html/contribuicao/php/getaway_pagamento.php">
+									<i class="fa-solid fa-building"></i>
+									Getaway de pagamento
+								</a>
+							</li>
+							<li >
+								<a href="<?= WWW ?>html/contribuicao/php/meio_pagamento.php">
+									<i class="fa-regular fa-credit-card"></i>
+									Meio de pagamento
+								</a>
+							</li>
+							<li >
+								<a href="<?= WWW ?>html/contribuicao/php/regras_pagamento.php">
+									<i class="fa-solid fa-circle-exclamation"></i>
+									Regras de pagamento
+								</a>
+							</li>
+						</ul>
+					</li>
+					<!--fim contribuiçao-->
+
 					<li class="nav-parent nav-active visivel">
 						<a>
 							<i class="fa fa-cog" aria-hidden="true"></i>
