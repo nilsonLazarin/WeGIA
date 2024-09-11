d52e209a13e4d5a2c4603ebd38bc0a228d8d4814
nicolly015 <nicollyboy01@gmail.com>
2024-09-11 16:06:36 -0300

    corrigindo issue [#372]

--- html/atendido/docfamiliar_upload.php
+++ html/atendido/docfamiliar_upload.php
@@ -15,25 +15,33 @@ if ($_POST){
     // A tabela funcioanrio_docs requer id_dependente, id_docdependente, extensao_arquivo, nome_arquivo e arquivo
     $arquivo = $_FILES["arquivo"];
     $nome_arquivo = $arquivo["name"];
-    $extensao_arquivo = explode(".", $arquivo["name"])[1];
+    $extensao_arquivo = strtolower(pathinfo($nome_arquivo, PATHINFO_EXTENSION));
     $arquivo_b64 = base64_encode(file_get_contents($arquivo['tmp_name']));
 
-    try {
-        $pdo = Conexao::connect();
-        $prep = $pdo->prepare("INSERT INTO funcionario_dependentes_docs (id_dependente, id_docdependentes, extensao_arquivo, nome_arquivo, arquivo) 
-        VALUES ( :idf , :idd , :ext , :n , COMPRESS(:a) )");
+    // Tipos de arquivos permitidos
+    $tipos_permitidos = ['pdf', 'jpg', 'jpeg', 'png', 'doc', 'docx'];
 
-        $prep->bindValue(":idf", $id_dependente);
-        $prep->bindValue(":idd", $id_docdependente);
-        $prep->bindValue(":ext", $extensao_arquivo);
-        $prep->bindValue(":n", $nome_arquivo);
-        $prep->bindValue(":a", $arquivo_b64);
+    // Verifica se a extensão do arquivo é permitida
+    if (in_array($extensao_arquivo, $tipos_permitidos)) {
+        try {
+            $pdo = Conexao::connect();
+            $prep = $pdo->prepare("INSERT INTO funcionario_dependentes_docs (id_dependente, id_docdependentes, extensao_arquivo, nome_arquivo, arquivo) 
+            VALUES ( :idf , :idd , :ext , :n , COMPRESS(:a) )");
 
-        $prep->execute();
-        
-        header("Location: ../profile_dependente.php?id_dependente=$id_dependente");
-    } catch (PDOException $e) {
-        echo("Houve um erro ao realizar o upload do documento:<br><br>$e");
+            $prep->bindValue(":idf", $id_dependente);
+            $prep->bindValue(":idd", $id_docdependente);
+            $prep->bindValue(":ext", $extensao_arquivo);
+            $prep->bindValue(":n", $nome_arquivo);
+            $prep->bindValue(":a", $arquivo_b64);
+
+            $prep->execute();
+            
+            header("Location: ../profile_dependente.php?id_dependente=$id_dependente");
+        } catch (PDOException $e) {
+            echo("Houve um erro ao realizar o upload do documento:<br><br>$e");
+        }
+    } else {
+        echo "Tipo de arquivo não permitido. Apenas arquivos PDF, JPG, JPEG, PNG, DOC e DOCX são aceitos.";
     }
 }else {
     header("Location: ../informacao_funcionario.php");
