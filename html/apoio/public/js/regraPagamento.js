45e19848f0e0e81872554cf2ce163f5b1d056331
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-09-24 10:36:10 -0300

    Transferência da página regra_pagamento.php para a nova estrutura de diretórios do módulo de contribuição [Issue #727]

---
+++ html/apoio/public/js/regraPagamento.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Seletor para todos os botões de editar
+    const editButtons = document.querySelectorAll('button[title="Editar"]');
+
+    editButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const id = this.getAttribute('data-id');
+            const valor = this.closest('tr').querySelector('td:nth-child(4)').textContent;
+
+            // Preenche o modal com os dados do gateway
+            document.getElementById('editId').value = id;
+            document.getElementById('editValor').value = valor;
+
+            $('#editModal').modal('show');
+        });
+    });
+
+    //Checkbox de ativar/desativar uma regra de pagamento
+    const toggles = document.querySelectorAll('.toggle-input');
+
+    toggles.forEach(toggle => {
+        toggle.addEventListener('change', function (ev) {
+            alterarStatus(ev, '../controller/control.php', 'RegraPagamentoController');
+        });
+    });
+});
