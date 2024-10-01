9c600723f705d7c5b3309c580ff81cf86d6fbb9e
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-10-01 08:17:40 -0300

    Implementação do método gerarNumeroDocumento em PagarMeBoletoService [Issue #726]

--- html/apoio/service/PagarMeBoletoService.php
+++ html/apoio/service/PagarMeBoletoService.php
@@ -3,14 +3,24 @@ require_once 'ApiBoletoServiceInterface.php';
 require_once '../model/ContribuicaoLog.php';
 class PagarMeBoletoService implements ApiBoletoServiceInterface{
     public function gerarBoleto(ContribuicaoLog $contribuicaoLog){
-        echo 'Entrou no serviço <br>';
-        echo 'Contribuição log: ';
-        print_r($contribuicaoLog);
-        echo '<br>Sócio: ';
-        print_r($contribuicaoLog->getSocio());
+        //gerar um número para o documento
+        $numeroDocumento = $this->gerarNumeroDocumento(16);
         return true;
     }
     public function guardarSegundaVia(){
 
+    }
+
+    /**
+     * Retorna um número com a quantidade de algarismos informada no parâmetro
+     */
+    public function gerarNumeroDocumento($tamanho){
+        $numeroDocumento = '';
+
+        for($i=0; $i < $tamanho; $i++){
+            $numeroDocumento .= rand(0,9);
+        }
+
+        return intval($numeroDocumento);
     }
 }
