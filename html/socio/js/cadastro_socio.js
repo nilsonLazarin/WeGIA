a2b0e3b22bd4aeb4c11a6b81999f41d8d03681fc
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-10-03 07:41:15 -0300

    Implementação do comportamento dos botões de voltar [Issue #744]

--- html/socio/js/cadastro_socio.js
+++ html/socio/js/cadastro_socio.js
@@ -1,9 +1,18 @@
+//Botões de avançar
 const btnAvancaValor = document.getElementById('avanca-valor');
 const btnAvancaCpf = document.getElementById('avanca-cpf');
 const btnAvancaContato = document.getElementById('avanca-contato');
 const btnAvancaPeriodo = document.getElementById('avanca-periodo');
 const btnAvancaEndereco = document.getElementById('avanca-endereco');
 
+//Botões de voltar
+const bntVoltaValor = document.getElementById('volta-valor');
+const btnVoltaCpf = document.getElementById('volta-cpf');
+const btnVoltaContato = document.getElementById('volta-contato');
+const bntVoltaPeriodo = document.getElementById('volta-periodo');
+const bntVoltaEndereco = document.getElementById('volta-endereco');
+
+//Definição do comportamento de avançar
 btnAvancaValor.addEventListener('click', (ev) => {
     ev.preventDefault();
     trocarPagina('pag2', 'pag1');
@@ -29,6 +38,38 @@ btnAvancaEndereco.addEventListener('click', (ev) => {
     trocarPagina('pag6', 'pag5');
 })
 
+//Definição do comportamento de voltar
+bntVoltaValor.addEventListener('click', (ev) =>{
+    ev.preventDefault();
+    trocarPagina('pag1', 'pag2');
+});
+
+btnVoltaCpf.addEventListener('click', (ev) =>{
+    ev.preventDefault();
+    trocarPagina('pag2', 'pag3');
+});
+
+btnVoltaContato.addEventListener('click', (ev) =>{
+    ev.preventDefault();
+    trocarPagina('pag3', 'pag4');
+});
+
+bntVoltaPeriodo.addEventListener('click', (ev) =>{
+    ev.preventDefault();
+    trocarPagina('pag4', 'pag5');
+});
+
+bntVoltaEndereco.addEventListener('click', (ev) =>{
+    ev.preventDefault();
+    trocarPagina('pag5', 'pag6');
+});
+
+/**
+ * Recebe como parâmetros a página para qual se deseja ir e a página atual,
+ * alterando qual das divs está visível no momento. 
+ * @param {*} idProxima 
+ * @param {*} idAtual 
+ */
 function trocarPagina(idProxima, idAtual){
     const atual = document.getElementById(idAtual);
     const proxima = document.getElementById(idProxima);
