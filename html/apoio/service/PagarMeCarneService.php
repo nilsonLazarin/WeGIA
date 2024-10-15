a98025299013fa0e45b04e8ccf46c980d9d0924e
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-10-15 10:08:23 -0300

    Criada geração do array de parcelas em JSON no método gerarCarne de PagarMeCarneService [Issue #749]

--- html/apoio/service/PagarMeCarneService.php
+++ html/apoio/service/PagarMeCarneService.php
@@ -1,15 +1,106 @@
-<?php 
+<?php
 require_once '../model/ContribuicaoLogCollection.php';
 require_once '../model/ContribuicaoLog.php';
 require_once 'ApiCarneServiceInterface.php';
-class PagarMeCarneService implements ApiCarneServiceInterface{
+class PagarMeCarneService implements ApiCarneServiceInterface
+{
     public function gerarCarne(ContribuicaoLogCollection $contribuicaoLogCollection)
     {
-        //Implementar geração de carnê
+        //definir constantes que serão usadas em todas as parcelas
+
+        $cpfSemMascara = preg_replace('/\D/', '', $contribuicaoLogCollection->getIterator()->current()->getSocio()->getDocumento()); //Ignorar erro do VSCode para método não definida em ->current() caso esteja utilizando intelephense
+
+        //Tipo do boleto
+        $type = 'DM';
+
+        //Validar regras
+
+        //Buscar Url da API e token no BD
+        try {
+            $gatewayPagamentoDao = new GatewayPagamentoDAO();
+            $gatewayPagamento = $gatewayPagamentoDao->buscarPorId(1); //Pegar valor do id dinamicamente
+
+            //print_r($gatewayPagamento);
+        } catch (PDOException $e) {
+            //Implementar tratamento de erro
+            echo 'Erro: ' . $e->getMessage();
+        }
+
+        //Buscar mensagem de agradecimento no BD
+        $msg = 'Agradecimento';
+        //Configurar cabeçalho da requisição
+        $headers = [
+            'Authorization: Basic ' . base64_encode($gatewayPagamento['token'] . ':'),
+            'Content-Type: application/json;charset=utf-8',
+        ];
+
+        //Montar array de parcelas
+        $parcela = [];
+
+        foreach ($contribuicaoLogCollection as $contribuicaoLog) {
+            //gerar um número para o documento
+            $numeroDocumento = $this->gerarNumeroDocumento(16);
+            $boleto = [
+                "items" => [
+                    [
+                        "amount" => $contribuicaoLog->getValor() * 100,
+                        "description" => "Donation",
+                        "quantity" => 1,
+                        "code" => $contribuicaoLog->getCodigo()
+                    ]
+                ],
+                "customer" => [
+                    "name" => $contribuicaoLog->getSocio()->getNome(),
+                    "email" => $contribuicaoLog->getSocio()->getEmail(),
+                    "document_type" => "CPF",
+                    "document" => $cpfSemMascara,
+                    "type" => "Individual",
+                    "address" => [
+                        "line_1" => $contribuicaoLog->getSocio()->getLogradouro() . ", n°" . $contribuicaoLog->getSocio()->getNumeroEndereco() . ", " . $contribuicaoLog->getSocio()->getBairro(),
+                        "line_2" => $contribuicaoLog->getSocio()->getComplemento(),
+                        "zip_code" => $contribuicaoLog->getSocio()->getCep(),
+                        "city" => $contribuicaoLog->getSocio()->getCidade(),
+                        "state" => $contribuicaoLog->getSocio()->getEstado(),
+                        "country" => "BR"
+                    ],
+                ],
+                "payments" => [
+                    [
+                        "payment_method" => "boleto",
+                        "boleto" => [
+                            "instructions" => $msg,
+                            "document_number" => $numeroDocumento,
+                            "due_at" => $contribuicaoLog->getDataVencimento(),
+                            "type" => $type
+                        ]
+                    ]
+                ]
+            ];
+
+            // Transformar o boleto em JSON e inserir no array de parcelas
+            $parcelas []= json_encode($boleto);
+        }
+
+        print_r($parcelas);
+
+        //Implementar requisição para API
+
+        return true;
     }
 
-    public function guardarSegundaVia()
+    public function guardarSegundaVia() {}
+
+     /**
+     * Retorna um número com a quantidade de algarismos informada no parâmetro
+     */
+    public function gerarNumeroDocumento($tamanho)//Transformar em utilitário
     {
-        
+        $numeroDocumento = '';
+
+        for ($i = 0; $i < $tamanho; $i++) {
+            $numeroDocumento .= rand(0, 9);
+        }
+
+        return intval($numeroDocumento);
     }
 }
