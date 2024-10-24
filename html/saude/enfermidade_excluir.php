0e5882d0f8bacd212dbc8df5945430b31d169088
dev <jgmppacheco@gmail.com>
2024-10-24 16:42:46 -0300

    Corrigindo Issue [#242]

--- html/saude/enfermidade_excluir.php
+++ html/saude/enfermidade_excluir.php
@@ -17,17 +17,26 @@ require_once "../../dao/Conexao.php";
 require_once "enfermidade.php";
 
 extract($_GET);
+$id_fichamedica = isset($_GET['id_fichamedica']) ? $_GET['id_fichamedica'] : null;
 
 $en = new EnfermidadeSaude($id_doc);
 if (!$en->getException()){
     $en->delete();
     
-    $sql = "SELECT se.id_CID, se.data_diagnostico, se.status, stc.descricao FROM saude_enfermidades se JOIN saude_tabelacid stc ON se.id_CID = stc.id_CID WHERE status = 1 AND id_fichamedica =" . $_GET['id_fichamedica'] . ";";
-    $pdo = Conexao::connect();
-    $enfermidades = $pdo->query($sql);
-    $enfermidades = $enfermidades->fetchAll(PDO::FETCH_ASSOC);
-    $enfermidades = json_encode($enfermidades);
-    echo $enfermidades;
+    try{
+        $sql = "SELECT se.id_CID, se.data_diagnostico, se.status, stc.descricao FROM saude_enfermidades se JOIN saude_tabelacid stc ON se.id_CID = stc.id_CID WHERE status = 1 AND id_fichamedica = ? ;";
+        
+        $pdo = Conexao::connect();
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(1, $id_fichamedica, PDO::PARAM_INT); // Bind do parâmetro
+        $stmt->execute();
+
+        $enfermidades = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $enfermidades = json_encode($enfermidades);
+        echo $enfermidades;
+    }catch(PDOException $e){
+        echo "Houve um erro ao realizar a exclusão da enfermidade:<br><br>" . htmlspecialchars($e->getMessage());
+    }
 }else{
     echo $en->getException();
 }
