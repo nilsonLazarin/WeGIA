ac6a2b938ebced5d7dc053ad5e1fbde5d629e205
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-11-04 10:58:30 -0300

    Adaptação do método criarCarne para que ele permita funcionários modificarem o dia de vencimento [Issue #787]

--- html/apoio/helper/Util.php
+++ html/apoio/helper/Util.php
@@ -26,7 +26,45 @@ class Util
     /**
      * Retorna apenas os números de um telefone
      */
-    public static function limpaTelefone(string $telefone){
+    public static function limpaTelefone(string $telefone)
+    {
         return preg_replace('/\D/', '', $telefone);
+    }
+
+    public static function mensalidadeInterna(int $intervalo, int $qtd_p, string $diaVencimento)
+    {
+        $datasVencimento = [];
+
+        if (empty($diaVencimento)) {
+            echo json_encode('O dia de vencimento de uma parcela não pode ser vazio');
+            exit();
+        }
+
+        $dia = explode('-', $diaVencimento)[2];
+
+        // Pegar a data informada
+        $dataAtual = new DateTime($diaVencimento);
+
+        // Iterar sobre a quantidade de parcelas
+        for ($i = 0; $i < $qtd_p; $i++) {
+            // Clonar a data atual para evitar modificar o objeto original
+            $dataVencimento = clone $dataAtual;
+
+            //incremento de meses
+            $incremento = $intervalo * $i;
+
+            // Adicionar os meses de acordo com o índice da parcela
+            $dataVencimento->modify("+{$incremento} month");
+
+            //verificar se o dia de dataVencimento é diferente de $dia, se forem diferentes
+            //subtrair um mês e modificar para o último dia
+            if ($dataVencimento->format('d') != $dia) {
+                $dataVencimento->modify('last day of previous month');
+            }
+
+            // Adicionar a data formatada ao array
+            $datasVencimento[] = $dataVencimento->format('Y-m-d');
+        }
+        return $datasVencimento;
     }
 }
--- html/socio/sistema/controller/script/geracao_auto_v3.js
+++ html/socio/sistema/controller/script/geracao_auto_v3.js
@@ -167,10 +167,10 @@ $(document).ready(function () {
 
                         switch(tipoGeracao){
                             case '0': url = '../../apoio/controller/control.php?nomeClasse=ContribuicaoLogController&metodo=criarBoleto'; break;
-                            case '1': url = '../../contribuicao/mensalidade/model/carne.php'; break; 
-                            case '2': url = '../../contribuicao/mensalidade/model/carne.php'; break; 
-                            case '3': url = '../../contribuicao/mensalidade/model/carne.php'; break; 
-                            case '6': url = '../../contribuicao/mensalidade/model/carne.php'; break; 
+                            case '1': url = '../../apoio/controller/control.php?nomeClasse=ContribuicaoLogController&metodo=criarCarne'; break; 
+                            case '2': url = '../../apoio/controller/control.php?nomeClasse=ContribuicaoLogController&metodo=criarCarne'; break; 
+                            case '3': url = '../../apoio/controller/control.php?nomeClasse=ContribuicaoLogController&metodo=criarCarne'; break; 
+                            case '6': url = '../../apoio/controller/control.php?nomeClasse=ContribuicaoLogController&metodo=criarCarne'; break; 
                             default: alert('O tipo de geração escolhido é inválido'); return;
                         }
 
@@ -187,7 +187,7 @@ $(document).ready(function () {
                             "documento_socio": cpfCnpj,
                             "valor": valor,
                             "dia": dia,
-                            "parcela": parcela,
+                            "parcelas": parcela,
                             "tipoGeracao": tipoGeracao
                         }).done(function (r) {
                             const resposta = JSON.parse(r);
