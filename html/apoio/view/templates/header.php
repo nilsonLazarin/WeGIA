3a10db18497163afece20033daaf9a07fce268aa
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-11-11 08:00:17 -0300

    Migração da página de segunda via [Issue #767]

--- html/apoio/view/templates/header.php
+++ html/apoio/view/templates/header.php
@@ -19,6 +19,8 @@ require_once "../../personalizacao_display.php";
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
+    <link rel="stylesheet" type="text/css" href="../public/css/index.css">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Bitter&display=swap" rel="stylesheet">
     <!--
