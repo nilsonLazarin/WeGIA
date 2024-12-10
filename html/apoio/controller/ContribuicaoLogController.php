8ec1d5dfc1b08638ebe0bea7551e9cf656fe1b7a
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-12-10 12:54:23 -0300

    Criação da página pix.php [Issue #767]

--- html/apoio/controller/ContribuicaoLogController.php
+++ html/apoio/controller/ContribuicaoLogController.php
@@ -332,7 +332,7 @@ class ContribuicaoLogController
      * Cria um objeto do tipo ContribuicaoLog, chama o serviço de pix registrado no banco de dados
      * e insere a operação na tabela de contribuicao_log caso o serviço seja executado com sucesso.
      */
-    public function criarQrCode()
+    public function criarQRCode()
     {
         $valor = filter_input(INPUT_POST, 'valor');
         $documento = filter_input(INPUT_POST, 'documento_socio');
@@ -412,9 +412,13 @@ class ContribuicaoLogController
             $socioDao->registrarLog($contribuicaoLog->getSocio(), $mensagem);
 
             //Chamada do método de serviço de pagamento requisitado
-            if (!$servicoPagamento->gerarQrCode($contribuicaoLog)) {
+
+            $codigoApi = $servicoPagamento->gerarQrCode($contribuicaoLog);
+
+            if (!$codigoApi) {
                 $this->pdo->rollBack();
             } else {
+                $contribuicaoLogDao->alterarCodigoPorId($codigoApi, $contribuicaoLog->getId());
                 $this->pdo->commit();
             }
         } catch (PDOException $e) {
---
+++ html/apoio/view/pix.php
@@ -0,0 +1,66 @@
+<?php
+$title = 'Emitir QRCode';
+require_once './templates/header.php';
+
+$textoTipoContribuicao = 'GERAR QRCode';
+$tipoContribuicao = 'QRCode';
+
+?>
+<div class="container-contact100">
+    <div class="wrap-contact100">
+
+        <!--Adiciona a logo e o título ao topo da página-->
+        <?php include('./components/contribuicao_brand.php'); ?>
+
+        <form id="formulario">
+
+            <input type="hidden" name="forma-contribuicao" id="forma-contribuicao" value="boleto">
+
+            <div id="pag1" class="wrap-input100">
+                <!--Adiciona a página de valor de contribuição-->
+                <?php include('./components/contribuicao_valor.php'); ?>
+                <?php $tipoAvanca = 'valor';
+                include('./components/btn_avanca.php'); ?>
+            </div>
+
+            <div id="pag2" class="wrap-input100 hidden">
+                <!--Adiciona a página para identificação de Sócios PJ e PF-->
+                <?php include('./components/contribuicao_documento.php'); ?>
+            </div>
+
+            <div id="pag3" class="wrap-input100 hidden">
+                <!--Adiciona a página para coleta do nome, data de nascimento, telefone e e-mail-->
+                <?php include('./components/contribuicao_contato.php'); ?>
+            </div>
+
+            <div id="pag4" class="wrap-input100 hidden">
+                <!--Adiciona a página para coleta do CEP, rua, número, bairro, estado, cidade e complemento-->
+                <?php include('./components/contribuicao_endereco.php'); ?>
+            </div>
+
+            <div id="pag5" class="wrap-input100 hidden">
+                <!--Adiciona a página para agradecimento e confirmação da geração do boleto-->
+                <?php include('./components/contribuicao_confirmacao.php'); ?>
+            </div>
+    </div>
+
+    </form>
+
+    <div class="wrap-contact100 mt-5 hidden text-center" id="qrcode-div">
+        <h4>Escaneie seu QRCode, <br> ou então clique no nosso copia e cola!</h4>
+       
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="../vendor/select2/select2.min.js"></script>
+    <script src="../public/js/mascara.js"></script>
+    <script src="../public/js/util.js"></script>
+    <script src="../public/js/pix.js"></script>
+    <!--Busca cep-->
+    <script src="../../../Functions/busca_cep.js"></script>
+    <?php
+    require_once './templates/footer.php';
+    ?>
