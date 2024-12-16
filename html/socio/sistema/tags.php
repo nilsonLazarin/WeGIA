f06f292a33c310d33674c8d8c793a734dc9b72a1
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-12-16 09:39:47 -0300

    Resolução XSS [Issue #833]

--- html/socio/sistema/tags.php
+++ html/socio/sistema/tags.php
@@ -190,7 +190,7 @@ $recurso = mysqli_query($conexao, "SELECT * FROM recurso");
 									while ($row = $tags->fetch_array(MYSQLI_ASSOC)) {
 										$id_tag = $row['id_sociotag'];
 										$tag = $row['tag'];
-										echo "<tr><td>$id_tag</td><td><input id='$id_tag' type='text' value='$tag'></td><td><a id='a_$id_tag' class='btn btn-primary' href='salvar_tag.php?id_tag=$id_tag&value=$tag' disabled>Salvar</a><td><a class='btn btn-danger' href='deletar_tag.php?id_tag=$id_tag'>Deletar</a></td></tr>";
+										echo "<tr><td>$id_tag</td><td><input id='$id_tag' type='text' value='$tag'></td><td><a id='a_$id_tag' class='btn btn-primary' href='salvar_tag.php?id_tag=$id_tag&value={htmlspecialchars($tag)}' disabled>Salvar</a><td><a class='btn btn-danger' href='deletar_tag.php?id_tag=$id_tag'>Deletar</a></td></tr>";
 									}
 									?>
 								</tbody>
