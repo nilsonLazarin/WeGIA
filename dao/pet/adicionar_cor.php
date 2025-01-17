ae9c859006143bd0087b3e6e48a0677e1fff5c7e
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-01-17 17:43:14 -0300

    Resolução SQL Injection [Security https://github.com/LabRedesCefetRJ/WeGIA/security/advisories/GHSA-h2mg-4c7q-w69v]

--- dao/pet/adicionar_cor.php
+++ dao/pet/adicionar_cor.php
@@ -1,10 +1,16 @@
 <?php
-require_once'../Conexao.php';
-$pdo = Conexao::connect();
-// $cargo = $_POST["nome_docfuncional"];
-$cor = $_POST["cor"];
+require_once '../Conexao.php';
+require_once '../../html/permissao/permissao.php';
+permissao($_SESSION['id_pessoa'], 6, 3);
 
-$sql = "INSERT INTO pet_cor(descricao) values('" .$cor ."')";
-$pdo->query($sql);
-
-?>
+$cor = filter_input(INPUT_POST, 'cor', FILTER_SANITIZE_STRING);
+try {
+    $pdo = Conexao::connect();
+    $sql = "INSERT INTO pet_cor(descricao) values(:cor)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':cor', $cor);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['erro' => 'Erro no servidor ao inserir a cor do pet.']);
+    exit();
+}
