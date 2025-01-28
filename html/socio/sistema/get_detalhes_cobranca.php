ed45f5e653498a0a720c49c871d76bf97478f608
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-01-28 12:25:12 -0300

    Resolução SQL Injection [Security https://github.com/LabRedesCefetRJ/WeGIA/security/advisories/GHSA-jpph-g9p7-9jrm]

--- html/socio/sistema/get_detalhes_cobranca.php
+++ html/socio/sistema/get_detalhes_cobranca.php
@@ -1,24 +1,41 @@
  <?php
+    session_start();
+    if (!isset($_SESSION['usuario'])) die("Você não está logado(a).");
+
+    require_once '../../permissao/permissao.php';
+    permissao($_SESSION['id_pessoa'], 4, 3);
+    
     require("../conexao.php");
-    if(!isset($_POST) or empty($_POST)){
-        $data = file_get_contents( "php://input" );
-        $data = json_decode( $data, true );
+    if (!isset($_POST) or empty($_POST)) {
+        $data = file_get_contents("php://input");
+        $data = json_decode($data, true);
         $_POST = $data;
-    }else if(is_string($_POST)){
+    } else if (is_string($_POST)) {
         $_POST = json_decode($_POST, true);
     }
     $conexao->set_charset("utf8");
     extract($_REQUEST);
-    $query = mysqli_query($conexao, "SELECT * FROM `cobrancas` c JOIN socio s ON c.id_socio = s.id_socio JOIN pessoa p ON s.id_pessoa = p.id_pessoa WHERE codigo = '$codigo'");
-    while($resultado = mysqli_fetch_assoc($query)){
-        $dados[] = $resultado;
+
+    $sql = "SELECT * FROM cobrancas c JOIN socio s ON c.id_socio = s.id_socio JOIN pessoa p ON s.id_pessoa = p.id_pessoa WHERE codigo = ?";
+
+    $stmt = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_bind_param($stmt, 's', $codigo);
+
+    mysqli_stmt_execute($stmt);
+
+    // Obter o resultado do statement
+    $resultado = mysqli_stmt_get_result($stmt);
+
+    while ($detalhes = mysqli_fetch_assoc($resultado)) {
+        $dados[] = $detalhes;
     }
 
-    // array_walk_recursive($dados, function (&$val) {
-    //     if (is_string($val)) {
-    //         $val = mb_convert_encoding($val, 'UTF-8', 'UTF-8');
-    //     }
-    // });
+    // Fechar o primeiro statement
+    mysqli_stmt_close($stmt);
+
+    // Fechar a conexão
+    mysqli_close($conexao);
 
     echo json_encode($dados);
-?>
+    ?>
--- html/socio/sistema/get_detalhes_socio.php
+++ html/socio/sistema/get_detalhes_socio.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+if (!isset($_SESSION['usuario'])) die("Você não está logado(a).");
+
+require_once '../../permissao/permissao.php';
+permissao($_SESSION['id_pessoa'], 4, 3);
+
+
 require("../conexao.php");
 if (!isset($_POST) or empty($_POST)) {
     $data = file_get_contents("php://input");
