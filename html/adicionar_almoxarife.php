ff07d0cb52b7205b701fa5c70de37041d26116f8
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-02-06 11:57:10 -0300

    Resolução SQL Injection [Security https://github.com/LabRedesCefetRJ/WeGIA/security/advisories/GHSA-9cwj-p4x6-pp88]

--- html/adicionar_almoxarife.php
+++ html/adicionar_almoxarife.php
@@ -1,30 +1,40 @@
 <?php
-    session_start();
-    require_once './permissao/permissao.php';
-    permissao($_SESSION['id_pessoa'], 91);
-    if (!isset($id_funcionario) && !isset($id_almoxarifado)){
-        header("Location: ./geral/editar_permissoes.php?msg=Preencha todos os campos antes de prosseguir!&flag=warn");
+session_start();
+require_once './permissao/permissao.php';
+permissao($_SESSION['id_pessoa'], 91);
+if (!isset($id_funcionario) && !isset($id_almoxarifado)) {
+    header("Location: ./geral/editar_permissoes.php?msg=Preencha todos os campos antes de prosseguir!&flag=warn");
+}
+extract($_REQUEST);
+
+
+if (!isset($_SESSION['id_pessoa'])) {
+    header("Location: ../index.php");
+}
+
+require_once '../dao/Conexao.php';
+$pdo = Conexao::connect();
+
+$stmt = $pdo->prepare("SELECT * FROM almoxarife WHERE id_funcionario=:idFuncionario AND id_almoxarifado=:idAlmoxarifado");
+
+$stmt->bindParam(':idFuncionario', $id_funcionario);
+$stmt->bindParam(':idAlmoxarifado', $id_almoxarifado);
+
+$stmt->execute();
+
+if ($stmt->rowCount() > 0) {
+    header("Location: ./geral/editar_permissoes.php?msg=Funcionário já cadastrado para o Almoxarifado escolhido!&flag=warn");
+} else {
+    try {
+        $stmt2 = $pdo->prepare("INSERT INTO almoxarife (id_funcionario, id_almoxarifado) VALUES (:idFuncionario , :idAlmoxarifado)");
+
+        $stmt2->bindParam(':idFuncionario', $id_funcionario);
+        $stmt2->bindParam(':idAlmoxarifado', $id_almoxarifado);
+
+        $stmt2->execute();
+
+        header("Location: ./geral/editar_permissoes.php?msg=Funcionário cadastrado como almoxarife!&flag=success");
+    } catch (PDOException $e) {
+        header("Location: ./geral/editar_permissoes.php?msg=Erro: &flag=erro&log={$e->getMessage()}");
     }
-    extract($_REQUEST);
-
-    
-    if (!isset($_SESSION['id_pessoa'])){
-        header("Location: ../index.php");
-    }
-
-    require_once '../dao/Conexao.php';
-    $pdo = Conexao::connect();
-
-    $almox = $pdo->query("SELECT * FROM almoxarife WHERE id_funcionario=$id_funcionario AND id_almoxarifado=$id_almoxarifado;")->fetch(PDO::FETCH_ASSOC);
-    if ($almox) {
-        header("Location: ./geral/editar_permissoes.php?msg=Funcionário já cadastrado para o Almoxarifado escolhido!&flag=warn");
-    }else{
-        try{
-            $pdo->exec("INSERT INTO almoxarife (id_funcionario, id_almoxarifado) VALUES ( $id_funcionario , $id_almoxarifado );");
-            header("Location: ./geral/editar_permissoes.php?msg=Funcionário cadastrado como almoxarife!&flag=success");
-        }catch (PDOException $e){
-            header("Location: ./geral/editar_permissoes.php?msg=Erro: &flag=erro&log=$e");
-        }
-    }
-
-?>
+}
